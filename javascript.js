047f74acb0afbd9c3fa91c27a67cd7c893591ea2
Adrian Davidson <31959515+AdrianDavidson@users.noreply.github.com>
2017-09-15 14:30:02 +0100

    Add files via upload
    Each file is still incomplete

---
+++ javascript.js
@@ -0,0 +1,57 @@
+// fade in welcome screen
+
+$.fn.center = function () {
+  this.css("position","absolute");
+  this.css("top", Math.max(0, (
+    ($(window).height() - $(this).outerHeight()) / 2) +
+     $(window).scrollTop()) + "px"
+  );
+  this.css("left", Math.max(0, (
+    ($(window).width() - $(this).outerWidth()) / 2) +
+     $(window).scrollLeft()) + "px"
+  );
+  return this;
+}
+$("#overlay").show();
+$("#overlay-content").show().center();
+
+setTimeout(function(){
+  $("#overlay").fadeOut();
+}, 3000);
+
+
+// makes slider slide up
+$('.trigger, .slider').click(function() {
+  $('.slider').toggleClass('close');
+});
+
+// $(function(){
+//
+// var appendthis =  ("<div class='modal-overlay js-modal-close'></div>");
+//
+//   $('a[data-modal-id]').click(function(e) {
+//     e.preventDefault();
+//     $("body").append(appendthis);
+//     $(".modal-overlay").fadeTo(500, 0.7);
+//     //$(".js-modalbox").fadeIn(500);
+//     var modalBox = $(this).attr('data-modal-id');
+//     $('#'+modalBox).fadeIn($(this).data());
+//   });
+//
+//
+// $(".js-modal-close, .modal-overlay").click(function() {
+//   $(".modal-box, .modal-overlay").fadeOut(500, function() {
+//     $(".modal-overlay").remove();
+//   });
+// });
+//
+// $(window).resize(function() {
+//   $(".modal-box").css({
+//     top: ($(window).height() - $(".modal-box").outerHeight()) / 2,
+//     left: ($(window).width() - $(".modal-box").outerWidth()) / 2
+//   });
+// });
+//
+// $(window).resize();
+//
+// });
